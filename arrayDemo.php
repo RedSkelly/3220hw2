3b9499c44d98bbdde897ea37be4e46867b79d819
mikekaylor98 <mikekaylor98@gmail.com>
2020-08-31 23:23:24 -0400

    table 3 done, start signange

--- arrayDemo.php
+++ arrayDemo.php
@@ -70,7 +70,6 @@
             for($i = 0; $i < $columns; $i++) {
                 
                 $sum += $numGen[$j][$i];
-
             }
 
             $avg = $sum / $columns;
@@ -79,11 +78,20 @@
 
             $dataGen[$j][2] = number_format($avg,3);
 
-            $stdDev1 = pow(($sum - ($avg * $columns)),2); // subtract mean from each value
-            $stdDev2 = $stdDev1 * (1/$columns); // 
-            $stdDev3 = sqrt($stdDev2);
+            // $stdDev1 = ($sum - ($avg * $columns)); // subtract mean from each value
+            // $stdDev2 = $stdDev1 * (1/$columns); // 
+            // $stdDev3 = sqrt($stdDev2);
 
-            $dataGen[$j][3] = number_format($stdDev3,3);
+            $variance = 0;
+            $stdDev = 0;
+            for($i = 0; $i < $columns; $i++) {
+
+                $variance += (pow(($numGen[$j][$i] - $avg), 2)) / ($columns - 1);
+            
+            }
+
+            $stdDev += sqrt($variance);
+            $dataGen[$j][3] = number_format($stdDev,3);
         }
         
 
@@ -94,6 +102,36 @@
             }
             print("</tr>");
         }
+        print("</table>");
+
+        print("<table border = '3' >");
+        for($x = 0; $x < $rows; $x++) {
+            print("<tr>");
+            for($y = 0; $y < $columns; $y++) {
+                print("<td align='center'>" . $numGen[$x][$y] . "</td>");
+                
+                if ($numGen[$x][$y] < 0) {
+                    
+                    print("<td align='center'>" . "negative" . "</td>");
+                
+                }
+                else if ($numGen[$x][$y] > 0) {
+                    
+                    print("<td align='center'>" . "positive" . "</td>");
+                
+                }
+                else if ($numGen[$x][$y] == 0) {
+                    
+                    print("<td align='center'>" . "zero" . "</td>");
+                
+                }
+                
+            }
+            print("</tr>");
+        }
+
+
+
         print("</table>");
     ?>
     <p>
